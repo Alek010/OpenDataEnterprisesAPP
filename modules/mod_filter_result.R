adc607e9f5358aec21f63cece2e1662b8c489054
Alek010 <aleksejs.ivanovskis@gmail.com>
2023-07-08 19:46:53 +0300

    module filterResultServer modified to be mo generic. Logic of data processing separated into two methods filter_df_based_on_classname and filter_df_per_value_based_on_class_name.

--- modules/mod_filter_result.R
+++ modules/mod_filter_result.R
@@ -1,95 +1,113 @@
 filterResultUI <- function(id) {
   ns <- NS(id)
-  tabPanel("Filters",
-           radioButtons(ns("filter_radio"),
-                        label = h3("Filter enterprise under insolvency proceeding"),
-                        choices = list("by enterprise registration number" = 1,
-                                       "by enterprise name" = 2),
-                        selected = 1,
-                        width = "150%"),
-           textInput(ns("filter_input_text"),
-                     label = "",
-                     width = "150%",
-                     placeholder = "For example 40003608302 or multiple values 40003608302;4000360800"),
-           actionButton(inputId = ns("action_button"), label = "Filter"),
-           p(),
-           h3("Result"),
-           tabsetPanel(type = "tabs",
-                       id = ns("result"),
-                       tabPanel("All in One",
-                                p(),
-                                DTOutput(ns("dt")))
-                       )
-
+  tabPanel(
+    "Filters",
+    radioButtons(ns("filter_radio"),
+      label = h3("Filter enterprise under insolvency proceeding"),
+      choices = list(
+        "by enterprise registration number" = 1,
+        "by enterprise name" = 2
+      ),
+      selected = 1,
+      width = "150%"
+    ),
+    textInput(ns("filter_input_text"),
+      label = "",
+      width = "150%",
+      placeholder = "For example 40003608302 or multiple values 40003608302;4000360800"
+    ),
+    actionButton(inputId = ns("action_button"), label = "Filter"),
+    p(),
+    h3("Result"),
+    tabsetPanel(
+      type = "tabs",
+      id = ns("result"),
+      tabPanel(
+        "All in One",
+        p(),
+        DTOutput(ns("dt"))
+      )
+    )
   )
-
 }
 
-filterResultServer <- function(id, object_data_frame){
-  moduleServer(
-    id,
-    function(input, output, session) {
+filterResultServer <- function(id, object_data_frame) {
+  moduleServer(id, function(input, output, session) {
+    filter_input_values <- reactiveVal(character(0))
 
-      filter_input_values <- reactiveVal(character(0))
+    observeEvent(input$action_button, {
+      object_instance <- object_data_frame
+      object_instance$process_data()
 
-      observeEvent(input$action_button,
-                   {
-                     enterprises_under_insolvency <- object_data_frame
-                     enterprises_under_insolvency$process_data()
+      filter_values <- vectorize_input_text(input$filter_input_text)
 
-                     filter_values <- vectorize_input_text(input$filter_input_text)
+      filtered_df <- filter_df_based_on_classname(
+        object_instance,
+        input$filter_radio,
+        filter_values
+      )
 
-                     filtered_df <- switch (input$filter_radio,
-                                            "1" = enterprises_under_insolvency$filter_by_enterprise_registration_number(filter_values),
-                                            "2" = enterprises_under_insolvency$filter_by_enterprise_name(filter_values)
-                     )
+      output$dt <- DT::renderDataTable(filtered_df)
 
-                     output$dt <- DT::renderDataTable(filtered_df)
+      if (length(filter_input_values()) > 1) {
+        values <- filter_input_values()
 
-                     if(length(filter_input_values()) > 1){
+        for (value in values) {
+          removeTab(inputId = "result", target = value)
+        }
+      }
 
-                       values <- filter_input_values()
+      filter_input_values(filter_values)
 
-                       for (value in values) {
+      if (length(filter_values) > 1) {
+        filtered_df_per_value <- NULL
 
-                         removeTab(inputId = "result", target = value)
-                       }
-                     }
+        for (i in 1:length(filter_values)) {
+          insertTab(
+            inputId = "result",
+            tabPanel(
+              filter_values[i],
+              DTOutput(NS(id, filter_values[[i]]))
+            ),
+            target = "All in One"
+          )
 
-                     filter_input_values(filter_values)
+          filtered_df_per_value[[i]] <- filter_df_per_value_based_on_classname(
+            object_instance,
+            input$filter_radio,
+            filtered_df,
+            filter_values[i]
+          )
 
-                     if( length(filter_values) > 1){
+          local({
+            local_df <- filtered_df_per_value[[i]]
 
-                       filtered_df_per_value <- NULL
+            output[[filter_values[i]]] <- DT::renderDataTable(local_df)
+          })
+        }
+      }
+    })
+  })
+}
 
-
-                           for (i in 1:length(filter_values)) {
-
-                           insertTab(inputId = "result",
-                                     tabPanel(filter_values[i],
-                                              DTOutput(NS(id, filter_values[[i]]))),
-                                     target = "All in One"
-                           )
-
-                             filtered_df_per_value[[i]] <-
-                               switch (input$filter_radio,
-                                       "1" = filtered_df %>%
-                                         dplyr::filter(debtor_registration_number == filter_values[i]),
-                                       "2" = filtered_df %>%
-                                         dplyr::filter((debtor_name %>% toupper()) %like% (filter_values[i] %>% toupper()))
-                                       )
-
-                             local({
-                               local_df <- filtered_df_per_value[[i]]
-
-                               output[[filter_values[i]]] <- DT::renderDataTable(local_df)
-                             })
-
-                           }
-                    }
-                     }
-                   )
-
-    }
+filter_df_based_on_classname <- function(r6_object_instance, input_id, filter_values) {
+  filtered_df <- switch(class(r6_object_instance)[1],
+    "EnterprisesUnderInsolvencyProceeding" = switch(input_id,
+      "1" = r6_object_instance$filter_by_enterprise_registration_number(filter_values),
+      "2" = r6_object_instance$filter_by_enterprise_name(filter_values)
+    )
   )
-  }
+
+  return(filtered_df)
+}
+
+filter_df_per_value_based_on_classname <- function(r6_object_instance, input_id, filtered_df, filter_value) {
+  filtered_df_per_value <- switch(class(r6_object_instance)[1],
+    "EnterprisesUnderInsolvencyProceeding" = switch(input_id,
+      "1" = filtered_df %>%
+        dplyr::filter(debtor_registration_number == filter_value),
+      "2" = filtered_df %>%
+        dplyr::filter((debtor_name %>% toupper()) %like% (filter_value %>% toupper()))
+    )
+  )
+}
