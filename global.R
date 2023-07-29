b132b041b44ce102620d8cbe9e0ffd1f60dde6cd
Alek010 <aleksejs.ivanovskis@gmail.com>
2023-07-29 22:15:06 +0300

    Sourced r6 classes.

--- global.R
+++ global.R
@@ -12,6 +12,8 @@ library(DT)
 library(dplyr)
 library(stringr)
 
+APP_VERSION <- "0.1.0"
+
 source(file = "./helpers/vectorize_input_text.R")
 
 source(file = "./modules/mod_filter_dataframe_tabpanel.R")
@@ -20,4 +22,7 @@ source(file = "./modules/mod_admin_files_read_log.R")
 source(file = "./modules/mod_admin_files_update.R")
 source(file = "./modules/mod_about_versioning.R")
 
-APP_VERSION <- "0.1.0"
+source(file = "./r6_classes/AppVersionsList.R", encoding = "UTF-8")
+source(file = "./r6_classes/AppVersionsDataFrame.R")
+
+
