ee42724e7068af1d46af0f6c2224deabf37db5ef
Alek010 <aleksejs.ivanovskis@gmail.com>
2023-07-30 06:45:49 +0300

    Text about the project extracted to separate html file. which is sourced in module aboutProject.

--- modules/mod_about_project.R
+++ modules/mod_about_project.R
@@ -3,15 +3,7 @@ aboutProjectTabPanelUI <- function(id) {
 
   tabPanel(
     title = "Project", value = id,
-    h2("Open Data Enterprises"),
-    br(),
-    h4("Under construction!!!"),
-    br(),
-    h4("The project is using official open data files from Register of Enterprises."),
-    h4("Now project is focused only on Register of Enterprises of the Republc of Latvia (https://data.gov.lv/dati/lv/organization/ur)."),
-    br(),
-    h4("Found bugs, need new features or enhancements"),
-    p(" - open issue on GitHub: https://github.com/Alek010/OpenDataEnterprisesAPP/issues"),
-    p("- write via email: opendataenterprisesapp@gmail.com .")
+    includeHTML(path = "./html/AboutProject.html"),
+    style='width: 1000px;'
   )
 }
