47cced24934dac762df5e9848b14edc5c9570c1e
Alek010 <aleksejs.ivanovskis@gmail.com>
2023-08-01 05:18:17 +0300

    Datatable AboutVersions is modified to be mo wide, have paginasion of 15 rows, sorted by date descending.

--- modules/mod_about_versioning.R
+++ modules/mod_about_versioning.R
@@ -5,7 +5,7 @@ aboutVersioningTabPanelUI <- function(id, latest_app_version) {
     title = "Version", value = "project_version",
     h3(paste("Latest version is", latest_app_version)),
     br(),
-    DTOutput(ns("dt"))
+    DTOutput(ns("dt"),width = "1200px")
   )
 }
 
@@ -14,7 +14,14 @@ aboutVersioningTabPanelServer <- function(id, df_app_versions) {
 
     output$dt <- DT::renderDT(DT::datatable(df_app_versions,
                                         rownames = FALSE,
-                                        escape = FALSE))
+                                        escape = FALSE,
+                                        options = list(
+                                          dom = 'lp',
+                                          pageLength = 15,
+                                          lengthMenu = c(15, 30, 50, 100),
+                                          order = list(list(1, 'desc'))
+                                        )
+                                        ))
   })
 }
 
